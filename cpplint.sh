849085f93cee4f6a8f4c8f5e93a52efe134e2aa5
Dylan Alvarez <helllo5d123@gmail.com>
2017-10-01 14:42:38 -0300

    make cpplint ignore anaconda's python version

--- cpplint.sh
+++ cpplint.sh
@@ -1,2 +1,2 @@
 #!/usr/bash
-python ./cpplint.py --filter=`cat filter_options` `find -regextype posix-egrep -regex '\.\/[^/]+\.(h|cpp)'`
+/usr/bin/python ./cpplint.py --filter=`cat filter_options` *.cpp *.h
